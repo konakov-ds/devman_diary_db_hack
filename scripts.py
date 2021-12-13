5890ee1fc90ddde43b56458423d99362356ee7a3
Mark Konakov <konakovmark@yandex.ru>
2021-12-13 09:29:50 +0300

    Add get_schoolkid func

--- scripts.py
+++ scripts.py
@@ -4,64 +4,63 @@ from datacenter.models import Schoolkid, Chastisement,\
 from django.shortcuts import get_object_or_404
 
 
+def get_schoolkid(student_name):
+        schoolkid = Schoolkid.objects.filter(
+            full_name__contains=student_name
+        )
+        if not schoolkid:
+            print('Student does not exist!')
+        elif len(schoolkid) > 1:
+            print(f'There are more than one student with name {student_name}')
+        else:
+            return schoolkid[0]
+
+
 def remove_chastisements(student_name):
-    schoolkid = get_object_or_404(
-        Schoolkid,
-        full_name__contains=student_name
-    )
-    if schoolkid:
-        chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-        chastisements.delete()
-        print(f'{student_name} chastisements successfully deleted!')
-    else:
-        print('Student does not exist!')
+    schoolkid = get_schoolkid(student_name)
+    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
+    print(f'{student_name} chastisements successfully deleted!')
 
 
 def fix_marks(student_name):
-    schoolkid = get_object_or_404(
-        Schoolkid,
-        full_name__contains=student_name
-    )
-    if not schoolkid:
-        print('Student does not exist!')
+    schoolkid = get_schoolkid(student_name)
+    schoolkid_bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
+    if schoolkid_bad_marks:
+        for mark in schoolkid_bad_marks:
+            mark.points = random.randint(4, 5)
+            mark.save()
+        print(f'{student_name} marks successfully changed!')
     else:
-        schoolkid_bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
-        if schoolkid_bad_marks:
-            for mark in schoolkid_bad_marks:
-                mark.points = random.randint(4, 5)
-                mark.save()
-                print(f'{student_name} marks successfully changed!')
-        else:
-            print(f'Bad marks do not find!')
+        print(f'Bad marks do not find!')
 
 
 def create_commendation(student_name, subject_title):
-    commendation_text = 'Хвалю!'
-    schoolkid = get_object_or_404(
-        Schoolkid,
-        full_name__contains=student_name
-    )
-    if not schoolkid:
-        print('Student does not exist!')
-    else:
-        subject = get_object_or_404(
-            Subject,
-            title=subject_title,
-            year_of_study=schoolkid.year_of_study
-        )
-        if not subject:
-            print('Subject does not exist')
-        else:
-            lessons = Lesson.objects.filter(subject=subject)
-            lesson = random.choice(lessons)
-            teacher = lesson.teacher
-            lesson_date = lesson.date
+    commendation_texts = ['Хвалю!', 'Отличная работа!',
+                          'Предложил оригинальное решение!',
+                          'Инициативный и ответственный!',
+                          'Подготовка на вышем уровне!']
+    schoolkid = get_schoolkid(student_name)
 
-            Commendation.objects.create(
-                text=commendation_text,
-                created=lesson_date,
-                schoolkid=schoolkid,
-                subject=subject,
-                teacher=teacher
-            )
-            print(f'{student_name} commendation successfully added!')
+    subject = get_object_or_404(
+        Subject,
+        title=subject_title,
+        year_of_study=schoolkid.year_of_study
+    )
+    if not subject:
+        print('Subject does not exist')
+    else:
+        lessons = Lesson.objects.filter(subject=subject)
+        lesson = random.choice(lessons)
+        teacher = lesson.teacher
+        lesson_date = lesson.date
+        commendation_text = random.choice(commendation_texts)
+
+        Commendation.objects.create(
+            text=commendation_text,
+            created=lesson_date,
+            schoolkid=schoolkid,
+            subject=subject,
+            teacher=teacher
+        )
+        print(f'{student_name} commendation successfully added!')
